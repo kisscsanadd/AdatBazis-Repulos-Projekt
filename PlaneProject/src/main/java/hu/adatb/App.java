4c3dcb6368917d6fcdb3a515b410b56ff40de5b4
Peter Szirbik <szirbikpeti@gmail.com>
2020-04-06 15:30:18 +0200

    Changed icon.

--- PlaneProject/src/main/java/hu/adatb/App.java
+++ PlaneProject/src/main/java/hu/adatb/App.java
@@ -6,6 +6,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class App extends Application {
             scene.getStylesheets().add(App.class.getResource("/css/" + styleName).toExternalForm());
         }
 
+        primaryStage.getIcons().add(new Image(App.class.getResourceAsStream("/pictures/icon.jpg")));
         primaryStage.setScene(scene);
         primaryStage.setTitle(title);
         primaryStage.show();
@@ -44,7 +46,7 @@ public class App extends Application {
         primaryStage = stage;
         try {
             System.out.println("\n"+ App.CurrentTime() + "Opened application");
-            StageDeliver("reg_or_login.fxml", "Repülőgép");
+            StageDeliver("reg_or_login.fxml", "Repülőjárat foglaló rendszer");
         } catch (IOException e) {
             Utils.showWarning("Nem sikerült megnyitni a főablakot");
             Platform.exit();
